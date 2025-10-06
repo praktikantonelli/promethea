5edb125c0fa2ec6a017279293fc190501adc4f13
Luca <luca.antonelli@gmx.ch>
2025-10-06 19:57:52 +0200

    added tauri store plugin -> use as config file

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,7 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut builder = tauri::Builder::default();
+    let mut builder = tauri::Builder::default().plugin(tauri_plugin_store::Builder::new().build());
     #[cfg(debug_assertions)]
     {
         let devtools = tauri_plugin_devtools::init();
