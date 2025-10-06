6eb4fea5658c6a790ff6b46e4fc43d628bb467fe
Luca <luca.antonelli@gmx.ch>
2025-10-06 20:57:54 +0200

    started working on config store

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,26 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use serde_json::json;
+use std::path::PathBuf;
+use tauri::{AppHandle, Wry};
+use tauri_plugin_store::StoreExt;
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("Failed to open key-value config")]
+    StoreAccess(#[from] tauri_plugin_store::Error),
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {name}! You've been greeted from Rust!")
+}
+
+#[tauri::command]
+fn set_library_path(app: AppHandle<Wry>, path: String) -> Result<(), Error> {
+    let path = PathBuf::from(path);
+    let store = app.store("promethea-config.json")?;
+    store.set("library-path", json!({"value": path}));
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -15,6 +34,11 @@ pub fn run() {
 
     builder
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let store = app.store("promethea-config.json")?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
