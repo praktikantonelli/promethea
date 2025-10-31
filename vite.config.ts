97465ceb3952c014955134420e3df97b92b53439
Luca <luca.antonelli@gmx.ch>
2025-10-31 22:49:39 +0100

    feat(ui): update dependencies and configs for React and shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
 
   // settings for shadcn/ui
   plugins: [react(), tailwindcss()],
@@ -24,17 +22,9 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-        protocol: "ws",
-        host,
-        port: 1421,
-      }
-      : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+});
