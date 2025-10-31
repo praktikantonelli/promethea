9906c67a968ac0ee6f654b236d65b52d8c974b5d
Luca <luca.antonelli@gmx.ch>
2025-10-31 21:07:19 +0100

    feat(ui): add shadcn/ui as dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 // @ts-expect-error process is a nodejs global
@@ -5,6 +7,14 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
+
+  // settings for shadcn/ui
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -17,10 +27,10 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
