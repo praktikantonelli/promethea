4a139e756a230c060438dfd8debbc0c3cca1843e
Luca <luca.antonelli@gmx.ch>
2025-11-03 07:18:22 +0100

    feat(ui): start with missing DB dialog

---
+++ src/features/no-database-dialog/index.tsx
@@ -0,0 +1,15 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog";
+
+export default function NoDatabaseDialog() {
+  return (
+    <Dialog>
+    </Dialog>
+  )
+}
