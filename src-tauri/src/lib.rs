c47d257bb942a027368e0aede85df160efefa5f3
Luca <luca.antonelli@gmx.ch>
2025-11-12 06:58:26 +0100

    chore(query): format SQL query string for better readability

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -104,39 +104,46 @@ impl From<anyhow::Error> for Error {
 async fn fetch_books(state: State<'_, AppState>) -> Result<Vec<BookRecord>, String> {
     let read_guard = state.db_pool.read().await;
     if let Some(pool) = &*read_guard {
-        let query = "WITH series_info AS (
-                    SELECT
-                        bsl.book,
-                        json_group_array(
-                            json_object(
-                                'series', s.name, 'sort', s.sort, 'volume', bsl.entry
-                            )
-                        ) series_and_volume
-                    FROM
-                        series AS s
-                        JOIN books_series_link bsl ON bsl.series = s.id
-                    GROUP BY
-                        bsl.book
-                ),
-                authors_info AS (
-                    SELECT
-                        json_group_array(a.name) authors,
-                        json_group_array(a.sort) authors_sort,
-                        bal.book
-                    FROM
-                        authors AS a
-                        JOIN books_authors_link bal ON a.id = bal.author
-                    GROUP BY
-                        bal.book
-                )
-                SELECT
-                    id AS book_id, title, sort, date_added, date_published, last_modified AS date_modified, number_of_pages, goodreads_id, authors, authors_sort, CASE WHEN series_and_volume IS NULL OR trim(series_and_volume) = '' THEN '[]' WHEN json_valid(series_and_volume) = 1 THEN series_and_volume ELSE '[]' END AS series_and_volume
-                FROM
-                    books
-                    LEFT JOIN series_info ON series_info.book = books.id
-                    JOIN authors_info ON authors_info.book = books.id
-                ORDER BY
-                    books.date_added ASC";
+        let query = "
+            WITH series_info
+                AS (SELECT bsl.book,
+                            Json_group_array(Json_object('series', s.NAME, 'sort', s.sort,
+                                            'volume',
+                                            bsl.entry)) series_and_volume
+                    FROM   series AS s
+                            JOIN books_series_link bsl
+                            ON bsl.series = s.id
+                    GROUP  BY bsl.book),
+                authors_info
+                AS (SELECT Json_group_array(a.NAME) authors,
+                            Json_group_array(a.sort) authors_sort,
+                            bal.book
+                    FROM   authors AS a
+                            JOIN books_authors_link bal
+                            ON a.id = bal.author
+                    GROUP  BY bal.book)
+            SELECT id            AS book_id,
+                title,
+                sort,
+                date_added,
+                date_published,
+                last_modified AS date_modified,
+                number_of_pages,
+                goodreads_id,
+                authors,
+                authors_sort,
+                CASE
+                    WHEN series_and_volume IS NULL
+                        OR Trim(series_and_volume) = '' THEN '[]'
+                    WHEN Json_valid(series_and_volume) = 1 THEN series_and_volume
+                    ELSE '[]'
+                END           AS series_and_volume
+            FROM   books
+                LEFT JOIN series_info
+                        ON series_info.book = books.id
+                JOIN authors_info
+                    ON authors_info.book = books.id
+            ORDER  BY books.date_added ASC  ";
         let books: Vec<BookRecord> = sqlx::query_as(query).fetch_all(pool).await.unwrap();
         return Ok(books);
     }
