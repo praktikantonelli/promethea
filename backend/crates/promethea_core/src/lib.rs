5ae3367de079560e5cb5df54faafa1fb4fb8ba38
luca <luca.antonelli@gmx.ch>
2026-01-23 18:03:01 +0100

    fix(clippy): fix grammatical error in docs

--- backend/crates/promethea_core/src/lib.rs
+++ backend/crates/promethea_core/src/lib.rs
@@ -1,6 +1,6 @@
 //! `promethea_core`
 //!
-//! Core library for the platform-independent logic of Promethea. This library aims to pose a crate
+//! Core library for the platform-independent logic of Promethea. This library aims to provide a crate
 //! that can be used both by a Tauri application and a HTTP server to avoid implementing the same
 //! logic twice.
 
