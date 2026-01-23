bf6047effa94f6327c9394cd6ba1f42588fb005e
luca <luca.antonelli@gmx.ch>
2026-01-23 15:18:36 +0100

    fix(clippy): add docs for App state

--- backend/crates/desktop/src/state.rs
+++ backend/crates/desktop/src/state.rs
@@ -8,18 +8,24 @@ pub const APP_CONFIG_PATH: &str = "promethea-config.json";
 /// Name of the `SQLite` database file that represents the library
 pub const LIBRARY_DATABASE_NAME: &str = "library.db";
 
+/// App state holding the database connection and the last error state
 pub struct AppState {
+    /// Used for all database queries, which are implemented on `Db`
     pub db: RwLock<Option<Db>>,
+    /// Holds the last error state arising from accessing the database
     pub last_error: RwLock<Option<String>>,
 }
 
 impl AppState {
+    /// Standard constructor
     pub fn new() -> Self {
         Self {
             db: RwLock::new(None),
             last_error: RwLock::new(None),
         }
     }
+
+    /// Takes an existing `AppState` instance and hooks it up with the provided path
     pub async fn connect_db_with_path(&self, path: PathBuf) -> anyhow::Result<()> {
         log::info!("Creating SQLite pool for DB at {}", path.display());
         let db = Db::init(&path).await?;
