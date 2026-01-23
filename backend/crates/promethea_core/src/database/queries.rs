e872661d1c64bb2921dd398056d17a79a5ed71c5
Luca <luca.antonelli@gmx.ch>
2026-01-23 06:46:53 +0100

    fix(clippy): add docs to queries

--- backend/crates/promethea_core/src/database/queries.rs
+++ backend/crates/promethea_core/src/database/queries.rs
@@ -3,10 +3,15 @@ use sqlx::{Sqlite, SqlitePool, Transaction, sqlite::SqliteConnectOptions};
 use std::path::Path;
 
 pub struct Db {
+    /// Used to execute queries with a persistent pool that is cheaply clonable.
     pool: SqlitePool,
 }
 
 impl Db {
+    /// Initialization function for database connection.
+    /// # Errors
+    /// This function fails if the `SQLite` database file cannot be accessed or if there's a problem
+    /// running the migrations on it.
     #[allow(
         clippy::missing_inline_in_public_items,
         reason = "Called once at start of program"
@@ -29,6 +34,10 @@ impl Db {
         self.pool.close().await;
     }
 
+    /// Function to fetch all books in the library. Performs multiple joins
+    /// # Errors
+    /// This function should generally NOT return an error, but if there happens to be a problem
+    /// with the database, that error is propagated upwards
     #[allow(
         clippy::missing_inline_in_public_items,
         reason = "Large function, called only when table updates"
@@ -93,6 +102,11 @@ impl Db {
         Ok(books)
     }
 
+    /// Queries the database to try and find an existing record on how to sort an author's name.
+    /// This is done so if an author requires a special sorting rule, that rule only has to be
+    /// defined once.
+    /// # Errors
+    /// This function errors only if there is a problem communicating with the database.
     #[allow(clippy::missing_inline_in_public_items, reason = "Called rarely")]
     pub async fn try_fetch_author_sort(&self, name: &str) -> Result<Option<String>, sqlx::Error> {
         let sort = sqlx::query!("SELECT sort FROM authors WHERE name LIKE ?", name)
@@ -103,6 +117,11 @@ impl Db {
         Ok(Some(sort))
     }
 
+    /// Queries the database to try and find an existing record on how to sort a series' name.
+    /// This is done so if a series requires a special sorting rule, that rule only has to be
+    /// defined once.
+    /// # Errors
+    /// This function errors only if there is a problem communicating with the database.
     #[allow(clippy::missing_inline_in_public_items, reason = "Called rarely")]
     pub async fn try_fetch_series_sort(&self, name: &str) -> Result<Option<String>, sqlx::Error> {
         let sort = sqlx::query!("SELECT sort FROM series WHERE name LIKE ?", name)
@@ -113,20 +132,27 @@ impl Db {
         Ok(Some(sort))
     }
 
+    /// Takes a `BookRecord` object and tries to insert it into the library of owned books. The
+    /// function works as follows:
+    /// 1. Insert just the book (title, sort, date added, date published, last modified, number of
+    ///    pages, goodreads id).
+    /// 2. Fetch the book's ID (succeeds if just created, otherwise warns about the book already
+    ///    existing).
+    /// 3. Insert the author(s)' name(s). Existing author records don't matter.
+    /// 4. Fetch author ID(s)
+    /// 5. Insert series. Existing series don't matter but existing series AND volume do.
+    /// 6. Fetch series ID(s)
+    /// 7. Insert into book and series link table.
+    /// 8. Insert into book and author link table.
+    /// # Errors
+    /// There are two ways in which this function call can fail:
+    /// 1. Problem with database
+    /// 2. Book already exists in database (only unique books allowed)
     #[allow(
         clippy::missing_inline_in_public_items,
         reason = "Called rarely, large function"
     )]
     pub async fn insert_book(&self, book: &BookRecord) -> Result<(), InsertBookError> {
-        // Query outline:
-        // 1. Insert book (title, sort, date_added, date_published, last_modified, number_of_pages, goodreads_id)
-        // 2. Fetch book ID (either newly created through operation 1 or already there and retrieved)
-        // 3. Insert author(s) (name, sort, goodreads_id)
-        // 4. Fetch author IDs (same principle as book ID)
-        // 5. Insert series (name, sort, volume, goodreads_id)
-        // 6. Fetch series IDs (same principle as books and authors)
-        // 7. Insert book series link (book ID, series ID(s))
-        // 8. Insert book authors link (book ID, author(s) ID(s))
         let mut tx: Transaction<'_, Sqlite> = self.pool.begin().await?;
 
         let book_goodreads_id = book.goodreads_id;
@@ -238,6 +264,8 @@ impl Db {
     }
 }
 
+/// Checks a returned `sqlx` error to see whether it is because of a unique constraint being
+/// violated by checking the error's attached message.
 #[allow(
     clippy::pattern_type_mismatch,
     reason = "False positive, this is the idiomatic pattern"
