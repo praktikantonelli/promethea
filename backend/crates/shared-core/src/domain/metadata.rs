bb21de0339ba5a540785380a030f8d4b9b429345
Luca <luca.antonelli@gmx.ch>
2026-03-05 06:50:29 +0100

    feat(domain): add metadata structs from previous implementation

--- backend/crates/shared-core/src/domain/metadata.rs
+++ backend/crates/shared-core/src/domain/metadata.rs
@@ -1,3 +1,33 @@
-pub struct BookRecord {}
-pub struct AuthorRecord {}
-pub struct SeriesAndVolumeRecord {}
+#[non_exhaustive]
+#[derive(Serialize, Debug, Deserialize, Clone, sqlx::FromRow)]
+pub struct BookRecord {
+    pub book_id: i64,
+    pub title: String,
+    pub sort: String,
+    #[sqlx(json)]
+    pub authors: Vec<AuthorRecord>,
+    #[sqlx(json)]
+    pub series_and_volume: Vec<SeriesAndVolumeRecord>,
+    pub number_of_pages: i64,
+    pub goodreads_id: i64,
+    pub date_added: NaiveDateTime,
+    pub date_published: NaiveDateTime,
+    pub date_modified: NaiveDateTime,
+}
+
+#[non_exhaustive]
+#[derive(Serialize, Debug, Deserialize, Clone, sqlx::FromRow)]
+pub struct AuthorRecord {
+    pub name: String,
+    pub sort: String,
+    pub goodreads_id: i64,
+}
+
+#[non_exhaustive]
+#[derive(Serialize, Debug, Deserialize, Clone, sqlx::FromRow)]
+pub struct SeriesAndVolumeRecord {
+    pub series: String,
+    pub sort: String,
+    pub volume: f64,
+    pub goodreads_id: i64,
+}
