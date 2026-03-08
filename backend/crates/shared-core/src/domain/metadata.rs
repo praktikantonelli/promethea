c2d9cd9048a872ddd7a4d3f7f4aa622a5dc77450
luca <luca.antonelli@gmx.ch>
2026-03-08 16:11:48 +0100

    feat(domain): add BookRecord methods

--- backend/crates/shared-core/src/domain/metadata.rs
+++ backend/crates/shared-core/src/domain/metadata.rs
@@ -18,6 +18,40 @@ pub struct BookRecord {
     pub date_modified: NaiveDateTime,
 }
 
+impl BookRecord {
+    #[allow(
+        clippy::too_many_arguments,
+        reason = "Constructor, cannot have fewer arguments"
+    )]
+    #[must_use]
+    #[inline]
+    pub const fn new(
+        book_id: i64,
+        title: String,
+        sort: String,
+        authors: Vec<AuthorRecord>,
+        series_and_volume: Vec<SeriesAndVolumeRecord>,
+        number_of_pages: i64,
+        goodreads_id: i64,
+        date_added: NaiveDateTime,
+        date_published: NaiveDateTime,
+        date_modified: NaiveDateTime,
+    ) -> Self {
+        Self {
+            book_id,
+            title,
+            sort,
+            authors,
+            series_and_volume,
+            number_of_pages,
+            goodreads_id,
+            date_added,
+            date_published,
+            date_modified,
+        }
+    }
+}
+
 #[non_exhaustive]
 #[derive(Serialize, Debug, Deserialize, Clone, sqlx::FromRow)]
 pub struct AuthorRecord {
