e1ec24197a4f2cd10e82c6863220af8db59d415f
Luca <luca.antonelli@gmx.ch>
2026-03-17 07:40:11 +0100

    fix(ports): move lint allow to trait, holds for all mehtods

--- backend/crates/shared-core/src/ports/database.rs
+++ backend/crates/shared-core/src/ports/database.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
+#[allow(async_fn_in_trait, reason = "Only used in my own code")]
 pub trait DatabasePort: Sized {
-    #[allow(async_fn_in_trait, reason = "Only used in my own code")]
     async fn init(path: &Path) -> Result<Self, sqlx::Error>; // TODO: replace sqlx:Error with own type
 }
