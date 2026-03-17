d0b7fbb40fe494c4888d44ccbb123a7f9ee1338f
Luca <luca.antonelli@gmx.ch>
2026-03-17 07:40:38 +0100

    feat(ports): add db close() method

--- backend/crates/shared-core/src/ports/database.rs
+++ backend/crates/shared-core/src/ports/database.rs
@@ -3,4 +3,6 @@ use std::path::Path;
 #[allow(async_fn_in_trait, reason = "Only used in my own code")]
 pub trait DatabasePort: Sized {
     async fn init(path: &Path) -> Result<Self, sqlx::Error>; // TODO: replace sqlx:Error with own type
+
+    async fn close(&self);
 }
