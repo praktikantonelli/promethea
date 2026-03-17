b0748cd048edf4815a7bca24e96337772c6a52e0
Luca <luca.antonelli@gmx.ch>
2026-03-17 07:38:01 +0100

    feat(ports): add database constructor

--- backend/crates/shared-core/src/ports/database.rs
+++ backend/crates/shared-core/src/ports/database.rs
@@ -1,1 +1,6 @@
-pub trait DatabasePort {}
+use std::path::Path;
+
+pub trait DatabasePort: Sized {
+    #[allow(async_fn_in_trait, reason = "Only used in my own code")]
+    async fn init(path: &Path) -> Result<Self, sqlx::Error>;
+}
