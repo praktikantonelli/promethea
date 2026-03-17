364f6132348a35207415979be18d1b30929b1594
Luca <luca.antonelli@gmx.ch>
2026-03-17 07:38:58 +0100

    fix(ports): add TODO comment about error types

--- backend/crates/shared-core/src/ports/database.rs
+++ backend/crates/shared-core/src/ports/database.rs
@@ -2,5 +2,5 @@ use std::path::Path;
 
 pub trait DatabasePort: Sized {
     #[allow(async_fn_in_trait, reason = "Only used in my own code")]
-    async fn init(path: &Path) -> Result<Self, sqlx::Error>;
+    async fn init(path: &Path) -> Result<Self, sqlx::Error>; // TODO: replace sqlx:Error with own type
 }
