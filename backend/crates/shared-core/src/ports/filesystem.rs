b01da3c94ea2f7718aac3e22a3f4d79719575cd0
luca <luca.antonelli@gmx.ch>
2026-03-30 11:04:46 +0200

    feat(filesystem): add file system port

--- backend/crates/shared-core/src/ports/filesystem.rs
+++ backend/crates/shared-core/src/ports/filesystem.rs
@@ -1,1 +1,14 @@
-pub trait FileSystemPort {}
+use std::path::Path;
+
+#[allow(async_fn_in_trait, reason = "Only used in my own code")]
+pub trait FileSystemPort {
+    async fn move_file(&self, source: &Path, target: &Path) -> Result<(), FileSystemError>;
+
+    async fn copy_file(&self, source: &Path, target: &Path) -> Result<(), FileSystemError>;
+
+    async fn delete_file(&self, path: &Path) -> Result<(), FileSystemError>;
+}
+
+/// Error with the file system
+#[derive(thiserror::Error, Debug)]
+enum FileSystemError {}
