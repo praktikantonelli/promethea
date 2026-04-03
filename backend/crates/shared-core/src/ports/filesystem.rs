548aa5f6212995d6d08fde74ed1a369025114c6e
luca <luca.antonelli@gmx.ch>
2026-04-03 08:45:40 +0200

    fix(filesystem): also need to create files

--- backend/crates/shared-core/src/ports/filesystem.rs
+++ backend/crates/shared-core/src/ports/filesystem.rs
@@ -2,6 +2,8 @@ use std::path::Path;
 
 #[allow(async_fn_in_trait, reason = "Only used in my own code")]
 pub trait FileSystemPort {
+    async fn create_file(&self, path: &Path) -> Result<(), FileSystemError>;
+
     async fn move_file(&self, source: &Path, target: &Path) -> Result<(), FileSystemError>;
 
     async fn copy_file(&self, source: &Path, target: &Path) -> Result<(), FileSystemError>;
