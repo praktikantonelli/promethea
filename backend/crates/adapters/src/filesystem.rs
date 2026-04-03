6ab8ba57b6cdb69c65f6d6f482fa0fd99e87948c
luca <luca.antonelli@gmx.ch>
2026-04-03 08:57:47 +0200

    feat(filesystem): add method signatures in adapter

--- backend/crates/adapters/src/filesystem.rs
+++ backend/crates/adapters/src/filesystem.rs
@@ -1,5 +1,14 @@
 use shared_core::ports::filesystem::FileSystemPort;
+use std::path::Path;
 
 pub struct FileSystem {}
 
-impl FileSystemPort for FileSystem {}
+impl FileSystemPort for FileSystem {
+    async fn create_file(&self, path: &Path) -> Result<(), FileSystemError> {}
+
+    async fn move_file(&self, source: &Path, target: &Path) -> Result<(), FileSystemError> {}
+
+    async fn copy_file(&self, source: &Path, target: &Path) -> Result<(), FileSystemError> {}
+
+    async fn delete_file(&self, path: &Path) -> Result<(), FileSystemError> {}
+}
