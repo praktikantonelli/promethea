71928026735982af1d4b4d604ea2a9b595c0edfb
luca <luca.antonelli@gmx.ch>
2026-04-05 23:02:48 +0200

    feat(usecase): add metadata provider field

--- backend/crates/shared-core/src/usecases/books.rs
+++ backend/crates/shared-core/src/usecases/books.rs
@@ -1,6 +1,7 @@
-use crate::ports::repository::BookRepositoryPort;
+use crate::ports::{metadata::MetadataProviderPort, repository::BookRepositoryPort};
 use std::sync::Arc;
 
 pub struct AddBookUseCase {
     repository: Arc<dyn BookRepositoryPort>,
+    metadata: Arc<dyn MetadataProviderPort>,
 }
