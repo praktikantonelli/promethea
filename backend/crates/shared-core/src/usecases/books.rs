e3f022005f67f1b4e86c66737dc75397e638315d
luca <luca.antonelli@gmx.ch>
2026-04-05 23:09:08 +0200

    feat(usecase): add execute() and necessary error

--- backend/crates/shared-core/src/usecases/books.rs
+++ backend/crates/shared-core/src/usecases/books.rs
@@ -5,3 +5,11 @@ pub struct AddBookUseCase {
     repository: Arc<dyn BookRepositoryPort>,
     metadata: Arc<dyn MetadataProviderPort>,
 }
+
+impl AddBookUseCase {
+    pub async fn execute(&self) -> Result<(), AddBookError> {}
+}
+
+#[derive(thiserror::Error, Debug)]
+#[non_exhaustive]
+pub enum AddBookError {}
