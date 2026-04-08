d6c13ff049d9fcbbe8773bffff2a7a6aea2bf3e7
luca <luca.antonelli@gmx.ch>
2026-04-08 11:08:50 +0200

    fix(filesystem): add empty implementations of new port methods

--- backend/crates/adapters/src/filesystem.rs
+++ backend/crates/adapters/src/filesystem.rs
@@ -11,4 +11,8 @@ impl FileSystemPort for FileSystem {
     async fn copy_file(&self, source: &Path, target: &Path) -> Result<(), FileSystemError> {}
 
     async fn delete_file(&self, path: &Path) -> Result<(), FileSystemError> {}
+
+    fn extract_title_from_epub(&self, path: &Path) -> Result<String, FileSystemError> {}
+
+    fn extract_author_from_epub(&self, path: &Path) -> Result<String, FileSystemError> {}
 }
