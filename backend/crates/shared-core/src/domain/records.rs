bff1e353b17a3bd4e185f8105a78157710298fab
luca <luca.antonelli@gmx.ch>
2026-04-12 12:55:23 +0200

    fix(domain): silence clippy errors

--- backend/crates/shared-core/src/domain/records.rs
+++ backend/crates/shared-core/src/domain/records.rs
@@ -99,6 +99,15 @@ impl SeriesAndVolumeRecord {
 pub struct GoodreadsId(i64);
 
 impl Display for GoodreadsId {
+    #[inline]
+    #[allow(
+        clippy::min_ident_chars,
+        reason = "signature is defined by Display trait"
+    )]
+    #[allow(
+        clippy::absolute_paths,
+        reason = "importing would override default Result"
+    )]
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
