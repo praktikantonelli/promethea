2c414b1b0be7705ab65969e4c88910e90b4d6b0d
Luca <luca.antonelli@gmx.ch>
2026-05-07 06:57:23 +0200

    feat(frontend): add toast component from shadcn/ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/app'
+import { Toaster } from '@/components/ui/sonner'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <Toaster />
+  </React.StrictMode>
 )
