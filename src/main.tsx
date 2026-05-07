2fcbe724b288d785517d04602bd39c9d0d551227
Luca <luca.antonelli@gmx.ch>
2026-05-07 07:52:22 +0200

    fix(app): wrap app and toaster with same theme, default system theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/app'
 import { Toaster } from '@/components/ui/sonner'
+import { ThemeProvider } from 'next-themes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </React.StrictMode>
 )
