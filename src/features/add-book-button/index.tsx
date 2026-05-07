9a2fd8b8393744d4035e8ccc91973e18540fa0f9
Luca Antonelli <79634977+praktikantonelli@users.noreply.github.com>
2026-05-07 07:58:52 +0200

    48 add error handling to frontend (#49)
    * feat(frontend): add toast component from shadcn/ui
    
    * feat(addbook): show status of adding book with a toast
    
    * fix(addbook): when dialog is cancelled, do not call backend with null path
    
    * fix(app): wrap app and toaster with same theme, default system theme

--- src/features/add-book-button/index.tsx
+++ src/features/add-book-button/index.tsx
@@ -3,6 +3,7 @@ import { useCallback } from "react";
 import { open as dialogOpen } from "@tauri-apps/plugin-dialog";
 import { invoke } from "@tauri-apps/api/core";
 import { info } from "@tauri-apps/plugin-log";
+import { toast } from "sonner";
 
 
 
@@ -10,7 +11,15 @@ export default function AddBookButton() {
   const handleAddNewBook = useCallback(async () => {
     info("Add new book button clicked");
     const path = await dialogOpen({ multiple: false, directory: false, filters: [{ name: "Book", extensions: ["epub"] }] });
-    await invoke("add_book", { path });
+
+    // if the user cancels, do nothing
+    if (!path || typeof path !== "string") {
+      return;
+    }
+
+    await invoke("add_book", { path })
+      .then(() => toast.success("Book successfully added"))
+      .catch((error) => toast.error(error));
   }, [])
 
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/app'
+import { Toaster } from '@/components/ui/sonner'
+import { ThemeProvider } from 'next-themes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <App />
+      <Toaster />
+    </ThemeProvider>
+  </React.StrictMode>
 )
