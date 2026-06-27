9271e6dfec2bcf725569e91d6de139befe33bc73
luca <luca.antonelli@gmx.ch>
2026-06-27 18:49:21 +0200

    fix(backend): use separate function for route, allows debug prints upon request

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -2,10 +2,15 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/hello", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/api/hello", get(say_hello));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn say_hello() -> &'static str {
+    println!("Request received!");
+    "Hello from axum!"
 }
