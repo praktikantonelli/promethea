8fc0b1c200283811ac4328c047343c95f880801c
Luca <luca.antonelli@gmx.ch>
2026-07-08 09:53:58 +0200

    fix(lib): add docs, add missing attributes

--- backend/shared/src/lib.rs
+++ backend/shared/src/lib.rs
@@ -1,4 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
+//! As-of-yet empty library crate for shared logic between desktop and web server backends
+///
+/// sample function from initializing library crate
+#[must_use]
+#[inline]
+#[allow(clippy::arithmetic_side_effects, reason = "unused sample function")]
+pub const fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
